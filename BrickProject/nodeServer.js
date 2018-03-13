163f2d120f747d58b47cb1b4cdcf347350e73966
TheRockstarr33 <drockstarr33@yahoo.com>
2018-03-13 21:40:42 +0400

    Updated Node Server

--- BrickProject/nodeServer.js
+++ BrickProject/nodeServer.js
@@ -11,34 +11,18 @@ http.createServer(function (req, res) {
 
 	console.log("");
 
+	if(req.url === "/favicon.ico") {
+		res.end("n");
+		return;
+	}
+
     var urlprs = url.parse(req.url, true);
-    //console.log("Pathname " + urlprs.pathname);
-    //console.log("Search " + urlprs.search);
-
     var query = urlprs.query;
-
     var numToAdd = urlprs.pathname.replace("/","");
 
-    console.log(numToAdd);
-    
     ticketNum += parseInt(numToAdd);
 
-    //ticketNum = ticketNum + parseInt(numToAdd);
-
-    //console.log(parseInt(query.n));
     console.log(ticketNum);
-
-    // if(query.n=="signup") {
-	   // if (signup(query.user, query.pass) == true) {
-    //         res.write("success");
-    //    }
-    // } else if(query.func=="login") {
-    //    if (login(query.user, query.pass) == true) {
-    //         res.write("true");
-    //    } else {
-    //         res.write("false");
-    //    }
-    // }
-
-    res.end("success");
+    
+    res.end(String(ticketNum));
 }).listen(8085);
